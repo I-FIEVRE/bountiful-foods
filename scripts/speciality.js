e57ad5b42126c395a3cfb9a9683435102f2967fc
I-FIEVRE <fievre.isabelle@gmail.com>
2023-07-17 18:38:40 +0200

    text

--- scripts/speciality.js
+++ scripts/speciality.js
@@ -3,7 +3,7 @@ const number = Number(window.localStorage.getItem("lastNumberDrink-ls")) || 0;
 if (number == '0') {
     numberDisplay.textContent = `You have not submitted any specialty drinks yet.`;
 } else if (number == '1') {
-		numberDisplay.textContent = `You have submitted ` + number + ` specialty drink since your last visit.`;
+		numberDisplay.textContent = `You have submitted ` + number + ` specialty drink.`;
 } else {
-		numberDisplay.textContent = `You have submitted ` + number + ` speciality drinks since your last visit.`;
+		numberDisplay.textContent = `You have submitted ` + number + ` speciality drinks.`;
 }
